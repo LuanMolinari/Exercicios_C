6f37399ada126eec08bd646f22888a15c5e10490
LuanMolinari <luanmolinari2015@gmail.com>
2024-03-23 20:33:57 -0300

    Alteração no exercicio 3
    mostrar os valores antes da troca

--- Lista_1/exe1.c
+++ Lista_1/exe1.c
@@ -1,3 +1,9 @@
+/*
+Faça um algoritmo para calcular o volume de uma lata cilíndrica através da seguinte fórmula:
+Vol = 3,14. r². h. O algoritmo deve receber o valor do raio (r) e da altura (h) e, após calcular o volume,
+mostrar o resultado ao usuário. Procure empregar o conceito de constantes e variáveis neste problema.
+*/
+
 #include <stdio.h>
 
     float PI = 3.14;
--- Lista_1/exe2.c
+++ Lista_1/exe2.c
@@ -1,3 +1,9 @@
+/*
+Escreva um algoritmo para ler dos valores para as variáveis A e B. O algoritmo deve efetuar a troca dos
+valores, de forma que a variável A passe a possuir o valor de B e a variável B passe a possuir o valor de
+A. O algoritmo deve apresentar os valores ao usuário, antes e depois da troca.
+*/
+
 #include <stdio.h>
 
 int main(){
@@ -9,11 +15,13 @@ int main(){
     printf ("\n Informe um valor para B: ");
     scanf ("%d", &b);
 
+    printf ("\n Valores antes da troca: \n A: %d \n B: %d\n", a, b);
+
     c = a;
     a = b;
     b = c;
 
-    printf ("\n Valores Trocados: \n A: %d \n B: %d\n", a, b);
+    printf ("\n Valores trocados: \n A: %d \n B: %d\n", a, b);
 
     return 0;
     
